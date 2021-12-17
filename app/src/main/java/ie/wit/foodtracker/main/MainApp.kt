689ab72de992ad14fc6db999a5c60d3a38157633
emma murphy <emmamurphy-103@hotmail.com>
2021-12-17 14:33:01 +0000

    Good features

--- app/src/main/java/ie/wit/foodtracker/main/MainApp.kt
+++ app/src/main/java/ie/wit/foodtracker/main/MainApp.kt
@@ -1,20 +1,21 @@
 package ie.wit.foodtracker.main
 import android.app.Application
+import ie.wit.foodtracker.models.EntryJSONStore
 import ie.wit.foodtracker.models.EntryMemStore
 import ie.wit.foodtracker.models.EntryModel
+import ie.wit.foodtracker.models.EntryStore
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainApp : Application() {
 
-   // val entrys = ArrayList<EntryModel>()
-   val entrys = EntryMemStore()
+    lateinit var entrys: EntryStore
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        
+        entrys = EntryJSONStore(applicationContext)
         i("Entry started")
-        //entrys.add(EntryModel("One", "About one..."))
-       // entrys.add(EntryModel("Two", "About two..."))
-      //  entrys.add(EntryModel("Three", "About three..."))
     }
 }
